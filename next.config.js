4039638d6074aa869478a075f159c7c3b1215a1c
devopsdbroe <dbroe1995@gmail.com>
2023-10-27 21:23:43 -0400

    Implemented Home Page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+			},
+		],
+	},
+	webpack(config) {
+		config.experiments = {
+			...config.experiments,
+			topLevelAwait: true,
+		};
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
